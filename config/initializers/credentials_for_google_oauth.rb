93f9d45a23db7dfe65dfc77b82db30774d20aa20
kwaters12 <kellywaters@gmail.com>
2014-08-06 22:29:09 -0700

    Fix2

--- config/initializers/credentials_for_google_oauth.rb
+++ config/initializers/credentials_for_google_oauth.rb
@@ -1,2 +1,2 @@
-ENV["GOOGLE_CLIENT_ID"] = "1028331010454-lbp1vtv58lfcdq2td4lv4kk8mjls3q5q.apps.googleusercontent.com"
-ENV["GOOGLE_CLIENT_SECRET"] = "Oka-10CGUsj_g71AmE4XM2Lv"
+# ENV["GOOGLE_CLIENT_ID"] = "1028331010454-lbp1vtv58lfcdq2td4lv4kk8mjls3q5q.apps.googleusercontent.com"
+# ENV["GOOGLE_CLIENT_SECRET"] = "Oka-10CGUsj_g71AmE4XM2Lv"
