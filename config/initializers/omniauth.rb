05511dbcb632c82187363d401b34d9ef146e6316
kwaters12 <kellywaters@gmail.com>
2014-08-07 11:50:22 -0700

    Fix email_vars.rb 2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
   OmniAuth.config.on_failure = HomepageController.action(:index)
-  provider :google_oauth2, 
-                    ENV['GOOGLE_CLIENT_KEY'], ENV['GOOGLE_CLIENT_SECRET'], { 
-                    access_type: "offline", 
-                    scope: 'https://www.googleapis.com/auth/userinfo.email' 
-                  }
 end
